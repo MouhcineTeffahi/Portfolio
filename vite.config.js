b8217056a70397e5e23bec3c7787b8dec75f6401
Mouhcine <mouhcine555@gmail..com>
2025-05-11 01:18:46 +0100

    Update images

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import compression from 'vite-plugin-compression'
 
-// https://vite.dev/config/
+// vite.config.js
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), // Activates the React plugin for Vite
+    compression() // Activates gzip compression for your production build
+  ],
+  build: {
+    chunkSizeWarningLimit: 1000, // Augments chunk size warning limit to 1MB
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Groups all code from node_modules into a single vendor chunk
+          }
+        }
+      }
+    }
+  }
 })
